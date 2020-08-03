37304eec6e762cef4413abd53db91d06924ca3a6
Koeppl, Benjamin <benjamin.koeppl.ex@teambank.de>
2020-08-03 15:18:38 +0200

    Add new Java class LoggingServiceImpl.java

---
+++ src/main/java/com/adorsys/springdemo/service/LoggingServiceImpl.java
@@ -0,0 +1,102 @@
+package de.isogmbh.top.kafkahttpbridge.service;
+
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoggingServiceImpl implements LoggingService {
+
+    Logger logger = LoggerFactory.getLogger("my-logger");
+
+    @Override
+    public void logRequest(HttpServletRequest httpServletRequest, Object body) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Map<String, String> parameters = buildParametersMap(httpServletRequest);
+
+        stringBuilder.append("REQUEST ");
+        stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
+        stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
+        stringBuilder.append("headers=[").append(buildHeadersMap(httpServletRequest)).append("] ");
+
+        if (!parameters.isEmpty()) {
+            stringBuilder.append("parameters=[").append(parameters).append("] ");
+        }
+
+        String bodyStr = "";
+        if (body != null) {
+            bodyStr = body.toString().replaceAll("\r", "").replaceAll("\n", "");
+        }
+        stringBuilder.append("body=[" + bodyStr + "]");
+
+        logger.info(maskString(stringBuilder.toString()));
+    }
+
+    @Override
+    public void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+            Object body) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("RESPONSE ");
+        stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
+        stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
+        stringBuilder.append("headers=[").append(buildHeadersMap(httpServletResponse)).append("] ");
+
+        String bodyStr = "";
+        if (body != null) {
+            bodyStr = body.toString().replaceAll("\r", "").replaceAll("\n", "");
+        }
+        stringBuilder.append("body=[").append(bodyStr).append("] ");
+
+        logger.info(maskString(stringBuilder.toString()));
+    }
+
+    private Map<String, String> buildParametersMap(HttpServletRequest httpServletRequest) {
+        Map<String, String> resultMap = new HashMap<>();
+        Enumeration<String> parameterNames = httpServletRequest.getParameterNames();
+
+        while (parameterNames.hasMoreElements()) {
+            String key = parameterNames.nextElement();
+            String value = httpServletRequest.getParameter(key);
+            resultMap.put(key, value);
+        }
+
+        return resultMap;
+    }
+
+    private Map<String, String> buildHeadersMap(HttpServletRequest request) {
+        Map<String, String> map = new HashMap<>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    private Map<String, String> buildHeadersMap(HttpServletResponse response) {
+        Map<String, String> map = new HashMap<>();
+
+        Collection<String> headerNames = response.getHeaderNames();
+        for (String header : headerNames) {
+            map.put(header, response.getHeader(header));
+        }
+
+        return map;
+    }
+
+    private String maskString(String str) {
+        return str.replaceAll("\"password\"\\s*:\\s*\"(.*?)\"", "\"password\": \"*****\"");
+    }
+}
